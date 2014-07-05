15ae1f7ed50033660a64305b56b38483a143b5b5
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2014-07-05 15:04:02 +0200

    using for each for construct an array of results instead of regular expressions. It was cool but useless

--- numberParser.js
+++ numberParser.js
@@ -1,12 +1,13 @@
 
 function findNumbers(text){
-    var writtenNumbers = { "un":1, "uno":1, "una":1, "dos":2, "par":2, "tres":3, "cuatro":4, "cinco":5, "seis":6, "siete":7, "ocho":8, "nueve":9, "diez":10, "once":11, "doce":12, "trece":13, "catorce":14, "quince":15 },
-    match,results = new Array();
-
-    for( number in writtenNumbers ){
-        var regexp = new RegExp(number,"ig");
-        while( match = regexp.exec( text )  )
-          { results.push(writtenNumbers[match]) }
-
-    }
+    var writtenNumbers = {"cero": 0, "un":1, "uno":1, "una":1, "dos":2, "par":2, "tres":3, "cuatro":4, "cinco":5, "seis":6, "siete":7, "ocho":8, "nueve":9, "diez":10, "once":11, "doce":12, "trece":13, "catorce":14, "quince":15 },
+    match,results = new Array(),Words=text.split(/[\s-]+/);
+    
+    
+    Words.forEach(function(number){
+    if( number in writtenNumbers ){
+        results.push(writtenNumbers[number])
+    }})
+    
+    return results;
 }
