81353bbd858351ed5619acf4f68797dde2cb22a1
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2014-07-05 16:53:44 +0200

    added named references. They are now available as local variables for subsecuent task

--- numberParser.js
+++ numberParser.js
@@ -1,6 +1,6 @@
 
 function findNumbers(text){
-    var writtenNumbers = {"cero": 0, "un":1, "uno":1, "una":1, "dos":2, "par":2, "tres":3, "cuatro":4, "cinco":5, "seis":6, "siete":7, "ocho":8, "nueve":9, "diez":10, "once":11, "doce":12, "trece":13, "catorce":14, "quince":15 },
+    var writtenNumbers = {"cero": 0, "un":1, "uno":1, "una":1, "dos":2, "par":2, "tres":3, "cuatro":4, "cinco":5, "seis":6, "siete":7, "ocho":8, "nueve":9, "diez":10, "once":11, "doce":12, "trece":13, "catorce":14, "quince":15,"dieciseis":16,"diecisiete":17,"dieciocho":18,"diecinueve":19,"veninte":20,"veintiuno":21,"veintidos":22,"veintitres":23,"veinticuatro":24,"veinticinco":25,"veintiseis":26,"veintisiete":27,"veintiocho":28,"veintinueve":29,"treinta":30, "cuarto":15,"media":30 },
     match,results = new Array(),Words=text.split(/[\s-]+/);
     
     
@@ -10,4 +10,18 @@ function findNumbers(text){
     }})
     
     return results;
+}
+
+namedRegexp(".*(?<year>[0-9]{4}).*"," el año 2014 mola");
+
+function namedRegexp(regexp,text){
+    regexp = XRegExp(regexp,"i");
+    var match = XRegExp.exec(text,regexp);
+    
+    //console.log("match ",match);
+    for( m in match){
+        if(typeof m !=="number")
+            setLocal(m,match[m]);
+    }
+    
 }
